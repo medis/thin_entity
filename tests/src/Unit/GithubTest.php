ba3fbe1f4d22ed254f9bd5a09c56c1c4d39b9d67
medis <aubuka@gmail.com>
2017-06-05 23:28:50 +0100

    add mock test

---
+++ tests/src/Unit/GithubTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Drupal\thin_entity\tests\Unit;
+
+use Drupal\Tests\UnitTestCase;
+
+class GithubTest extends UnitTestCase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    parent::setUp();
+  }
+
+  /**
+   * ::covers Github tests.
+   */
+  public function testGithub() {
+    $this->asserTrue(true);
+  }
+}
